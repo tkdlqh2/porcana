7fdc53574fe8388756e3c99ed4c1caf6f697d9b8
tkdlqh2 <84146296+tkdlqh2@users.noreply.github.com>
2026-01-21 15:15:55 +0900

    Fix: CurrentUser hidden 으로 변경

--- src/main/java/com/porcana/global/security/CurrentUser.java
+++ src/main/java/com/porcana/global/security/CurrentUser.java
@@ -1,9 +1,12 @@
 package com.porcana.global.security;
 
+import io.swagger.v3.oas.annotations.Parameter;
+
 import java.lang.annotation.*;
 
 @Target(ElementType.PARAMETER)
 @Retention(RetentionPolicy.RUNTIME)
 @Documented
+@Parameter(hidden = true)
 public @interface CurrentUser {
 }
